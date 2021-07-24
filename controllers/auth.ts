ee3172c68f038a870396f031ecca7e5f310e1b4a
Shawn <shawn2798@icloud.com>
2021-07-24 18:15:02 +0800

    convert to 1 query

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -8,25 +8,24 @@ export const createOrFindUser: RequestHandler = async (req, res, next) => {
     const { firebaseUser } = res.locals
     if (!firebaseUser) throw new Error()
 
-    const user = await User.findOne({ where: { email: firebaseUser.email } })
-    // No user if it does not exist in the DB
-    if (!user) {
-      const userId = generateUUID()
-      const newUser = await User.create({
+    const userId = generateUUID()
+    const [user, created] = await User.findOrCreate({
+      where: { email: firebaseUser.email },
+      defaults: {
         id: userId,
         name: firebaseUser.name,
         firebaseUserId: firebaseUser.firebaseUserId,
         email: firebaseUser.email,
         role: ROLES.EXECUTIVE,
         status: STATUS.PENDING
-      })
-      return res.status(201).json(newUser)
-    } else {
-      // User exists > next (Eg: when user refreshes the page)
-      res.status(200).json(user)
-      next()
-      // return res.status(400).json({ err: 'USER_EXISTED' })
-    }
+      }
+    })
+    // If user has just been created
+    if (created) return res.status(201).json(user)
+    // User exists > next (Eg: when user refreshes the page)
+    res.status(200).json(user)
+    next()
+    // return res.status(400).json({ err: 'USER_EXISTED' })
   } catch (err) {
     console.log('err', err)
     return res.status(401).json({ err: 'INVALID_OR_EXPIRED_TOKEN' })
