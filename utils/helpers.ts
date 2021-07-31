cce0ab349efb7fb39ea818ef278be14bfc0ef841
Choor Hong <abner.hong@hotmail.com>
2021-07-31 12:20:04 +0800

    Edit utils func

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,12 +1,18 @@
 import { v4 as uuidv4 } from 'uuid'
-
+import { isDate } from 'lodash'
 export const generateUUID = () => uuidv4()
+
+/**
+ *
+ * @param undefined, null, {}, [], '' > true
+ * @returns boolean
+ */
 
 export const checkEmpty = <T>(value: T) => {
   if (
     value === undefined ||
     value === null ||
-    (typeof value === 'object' && Object.keys(value).length === 0) ||
+    (typeof value === 'object' && Object.keys(value).length === 0 && !isDate(value)) ||
     (typeof value === 'string' && value.trim().length === 0)
   ) return true
   return false
