f46af1197543ac7492c48d29968a72f0cf37fae9
Shawn <shawn2798@icloud.com>
2021-08-08 15:51:02 +0800

    shipment pagination

--- controllers/shipment.ts
+++ controllers/shipment.ts
@@ -1,4 +1,5 @@
 import { RequestHandler } from 'express'
+import moment from 'moment'
 import { Op } from 'sequelize'
 import Booking from '../db/models/booking'
 import Contact from '../db/models/contact'
@@ -108,9 +109,28 @@ export default class ShipmentController extends BaseController {
     }
   }
 
+  /**
+   * Use like '/shipment/?cutOff=2021-08-10T07:28:04.204Z&next=true' or '/shipment/?cutOff=2021-08-10T07:28:04.204Z&previous=true'
+   * if next is true it will get next week's date from cutOff, if previous is true it will get last week's date from cutOff
+   * For example: cutOff = '2021-08-10T07:28:04.204Z' and next = true, it will query from 2021-08-16 to 2021-08-22
+   */
   public getAll: RequestHandler = async (req, res) => {
+    const { cutOff, previous, next } = req.query
+    let start = weekStart
+    let end = weekEnd
+    if (cutOff) {
+      const cutOffDate = cutOff.toString()
+      if (previous) {
+        start = moment(cutOffDate).subtract(1, 'week').startOf('isoWeek')
+        end = moment(cutOffDate).subtract(1, 'week').endOf('isoWeek')
+      } else if (next) {
+        start = moment(cutOffDate).add(1, 'week').startOf('isoWeek')
+        end = moment(cutOffDate).add(1, 'week').endOf('isoWeek')
+      }
+    }
+    let shipments
     try {
-      const shipments = await Shipment.findAll({
+      shipments = await Shipment.findAll({
         include: [
           { model: PurchaseOrder },
           { model: Contact, as: 'vendor' },
@@ -120,7 +140,7 @@ export default class ShipmentController extends BaseController {
               model: Vessel,
               where: {
                 cutOff: {
-                  [Op.between]: [weekStart, weekEnd]
+                  [Op.between]: [start, end]
                 } as any
               },
               required: true
@@ -130,6 +150,7 @@ export default class ShipmentController extends BaseController {
         ],
         order: [['booking', 'vessel', 'cutOff', 'ASC']]
       })
+      if (!shipments) shipments = await Shipment.findAll()
       if (!shipments) return this.notFound(res)
       return this.ok(res, shipments)
     } catch (error) {
