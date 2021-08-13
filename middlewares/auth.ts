aac857d7bf732a84924addc1eded722ab7c0fd3d
Choor Hong <abner.hong@hotmail.com>
2021-08-14 00:03:59 +0800

    Add getUserRoleStatus auth middleware

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -1,7 +1,9 @@
 import { RequestHandler } from 'express'
 import { BaseController } from '../controllers/base'
+import User from '../db/models/user'
 import admin from '../firebase'
 import { ErrorMessage } from '../types/error'
+import { STATUS } from '../types/user'
 
 export default class AuthMiddlewareController extends BaseController {
   public verifyToken: RequestHandler = async (req, res, next) => {
@@ -22,6 +24,34 @@ export default class AuthMiddlewareController extends BaseController {
     } catch (err) {
       console.log('err', err)
       return this.unauthorized(res, ErrorMessage.INVALID_OR_EXPIRED_TOKEN)
+    }
+  }
+
+  public getUserRoleStatus: RequestHandler = async (req, res, next) => {
+    const { firebaseUser } = res.locals
+    if (!firebaseUser) throw new Error()
+
+    try {
+      const user = await User.findOne({ where: { email: firebaseUser.email } })
+      if (!user) return this.notFound(res)
+
+      // account suspended >> subsequent data request will be rejected
+      if (user.status === STATUS.SUSPENDED) {
+        return this.unauthorized(res)
+      }
+
+      // account suspended >> subsequent data request will be rejected until account get approved > 'ACTIVE'
+      if (user.status === STATUS.PENDING) {
+        return this.unauthorized(res, 'PENDING_ACCOUNT_APPROVAL')
+      }
+
+      res.locals.userRoleStatus = {
+        // status: user.status // assume user status is ONLY active (STATUS.ACTIVE) at this point
+        role: user.role
+      }
+      next()
+    } catch (error) {
+      return this.fail(res, error)
     }
   }
 }
